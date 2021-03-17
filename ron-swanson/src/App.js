764f3d1033a44b045a790b0e1b33ea0a83af253f
ahmedseragcodes <ahmedserag1@gmail.com>
2021-03-17 17:27:29 -0400

    finished styling

--- ron-swanson/src/App.js
+++ ron-swanson/src/App.js
@@ -16,11 +16,18 @@ class App extends React.Component {
   render(){
     return (
       <div className="frontPageCatchAllContainer">
+        {this.props.isLoading ? (
+        <div>
+        <h1 className="loadingMessage">{this.state.quote}</h1>
         <img src="https://www.gannett-cdn.com/-mm-/eec6fd4cafba6c9b20fad3b0475b2dc4dc63a0e1/c=0-50-534-350/local/-/media/2016/08/05/Henderson/ronswanson.JPG?width=534&height=267&fit=crop&format=pjpg&auto=webp" alt="Ron Swanson From The Show Parks and Recreation" />
-        {this.props.isLoading ? <h1>{this.state.quote}</h1> : (
-          <div>
-          <h1>Listen to Ron Swanson</h1>
-          <p>{this.props.quote}</p>
+        </div>
+        )
+        : (
+          <div className="contentCard">
+          <h1>Wisdom From Ron Swanson</h1>
+          <img src="https://www.gannett-cdn.com/-mm-/eec6fd4cafba6c9b20fad3b0475b2dc4dc63a0e1/c=0-50-534-350/local/-/media/2016/08/05/Henderson/ronswanson.JPG?width=534&height=267&fit=crop&format=pjpg&auto=webp" alt="Ron Swanson From The Show Parks and Recreation" />
+          <p className="actualQuote">{this.props.quote}</p>
+          <button onClick={this.props.fetchData}>Hear Something New</button>
           </div>
         )}
       </div>
